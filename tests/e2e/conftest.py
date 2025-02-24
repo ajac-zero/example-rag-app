509d7307ccc398c5d84a21c1f0cd8d4e6b38a506
ajac-zero <ajcardoza2000@gmail.com>
2025-02-24 11:30:32 -0600

    added compose testcontainers

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,14 +1,7 @@
-import subprocess
-import time
-
 import pytest
+from testcontainers.compose import DockerCompose
 
 @pytest.fixture(scope="module")
 def compose():
-    #TODO: Replace this fixture with testcontainer docker compose; The documentation is non-existent though ;(
-    subprocess.run(["docker", "compose", "up", "-d", "litellm", "mockai"])
-    time.sleep(5)
-
-    yield
-
-    subprocess.run(["docker", "compose", "down"])
+    with DockerCompose(".") as compose:
+        yield compose
--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -5,12 +5,12 @@ from rag.entrypoints.rest import app
 
 client = TestClient(app)
 
-def test_send_messages():
+def test_send_messages(compose):
     response = client.post("/chat", json={"model": "mock", "messages": [{"role": "user", "content": "Hello, world!"}]}, params={"stream": False})
     assert response.status_code == 200
     assert response.json() == {"response": "Hello, world!"}
 
-def test_send_messages_stream():
+def test_send_messages_stream(compose):
     with client.stream("POST", "/chat", json={"model": "mock", "messages": [{"role": "user", "content": "Hello, world!"}]}, params={"stream": True}) as response:
         assert response.status_code == 200
 
